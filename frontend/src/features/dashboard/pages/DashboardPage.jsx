5a6ea58a2ed3b5a695dabbb9637afce0c7fe12d0
nooraaltmimi204-sketch <nooraaltmimi204@gmail.com>
2026-05-25 13:48:23 +0300

    Finished dashboard tickets UI

--- frontend/src/features/dashboard/pages/DashboardPage.jsx
+++ frontend/src/features/dashboard/pages/DashboardPage.jsx
@@ -1,11 +1,33 @@
-// src/features/dashboard/pages/DashboardPage.jsx
+
+import { Statiscs } from "../components/Statistics.jsx";
+import "../components/Statistics.css";
+import "../components/TicketCard.css"
 import React from "react";
+import { TicketCard } from "../components/TicketCard.jsx";
 
 const DashboardPage = () => {
   return (
-    <div className="p-8">
-      <h1 className="text-2xl font-bold">لوحة التحكم</h1>
-      <p>هنا تعرض التذاكر والإحصائيات الخاصة بالدعم الفني.</p>
+    <div className="dashboard">
+    <div className="Statiscs-section">
+      <Statiscs title="Active Ticket" number = "47" percent= "+12% from yesterday" icon="fa-solid fa-ticket"/>
+      <Statiscs title="Total Customers" number = "127" percent= "+8% from yesterday" icon= "fa-solid fa-users"/>
+      <Statiscs title="Revenue Today" number = "13,150$" percent= "+15% from yesterday" icon= "fa-dollar-sign"/>
+    </div>
+    <div className="tickets-container">
+
+        <div className="ticket-header">
+
+          <h4>Ticket</h4>
+          <h4>CreationDate</h4>
+          <h4>Assignie</h4>
+          <h4>Status</h4>
+
+        </div>
+
+           <TicketCard title= "download problem" description = "I can't download office programs" icon = "fa-solid fa-ticket" creationDate= "1/6/2026" assignee= "software" status="Pending"/>
+
+    
+        </div>
     </div>
   );
 };
